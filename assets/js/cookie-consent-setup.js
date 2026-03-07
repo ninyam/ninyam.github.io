a7e60e9f39d347b6b47fe7afec5ece48c7950521
ninyam <66387687+ninyam@users.noreply.github.com>
2026-03-07 03:19:50 +0000

    Deploying to gh-pages from @ ninyam/mywebsite@48f30a9de4e308f604d6b7a3ad2cd6e994b6ed79 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://ninyam.github.io/mywebsite//#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://ninyam.github.io/mywebsite//mywebsite/#contact">contact us</a>.'
               }
             ]
           }
